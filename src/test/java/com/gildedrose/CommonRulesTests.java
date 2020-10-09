685334677fbfeb79f20e2ec170112231686a5e45
Ruta9 <rutule9@gmail.com>
2020-10-09 17:40:43 +0300

    add more unit tests

--- src/test/java/com/gildedrose/CommonRulesTests.java
+++ src/test/java/com/gildedrose/CommonRulesTests.java
@@ -9,15 +9,19 @@ public class CommonRulesTests {
     @Test
     void qualityCannotExceed50() {
         Item[] items = new Item[] {
-                new Item("Backstage passes to a TAFKAL80ETC concert", 5, 50),
+                new Item("Backstage passes to a TAFKAL80ETC concert", 5, 49),
+                new Item("Backstage passes to a TAFKAL80ETC concert", 10, 49),
+                new Item("Backstage passes to a TAFKAL80ETC concert", 25, 50),
                 new Item("Aged Brie", 5, 50),
-                new Item("Aged Brie", -2, 50)
+                new Item("Aged Brie", -2, 49)
         };
         GildedRose app = new GildedRose(items);
         app.updateQuality();
         assertEquals(50, app.items[0].quality);
         assertEquals(50, app.items[1].quality);
         assertEquals(50, app.items[2].quality);
+        assertEquals(50, app.items[3].quality);
+        assertEquals(50, app.items[4].quality);
     }
 
     @Test
